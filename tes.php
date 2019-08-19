4af7ffbfbd95c50ad19e2f395dabedecd0e31289
as-segaf <ziyanassegaf20@gmail.com>
2019-08-19 08:48:45 +0700

    intial commit

---
+++ tes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<style>
+body
+{
+    background-color : #fff2cc;
+}
+p span b
+{
+    background-color: #7CFC00;
+    padding: 4px 3px;
+}
+</style>
+<body>
+    <h1>Ini elemen judul</h1>
+    <p>Ini <b>elemen</b> paragraf dengan <span>hig <b>hl</b>ight</span> pada kata tertentu.</p>
+</body>
+</html>
